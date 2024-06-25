960cf1978332b7e5b23b98735527378dcbc87276
VanshikaAggarwal21 <vanshika151.be21@chitkara.edu.in>
2024-06-25 18:04:42 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from "cors"
+const app=express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true , limit:"16kb"}))
+app.use(express.static("public"))// for static folder upload like favicon etc 
+app.use(cookieParser())
+
+export {app}
+
+//Cookie parsing is a process of extracting and handling data stored in cookies,  Cookies are small pieces of data that websites store on a user's browser 
